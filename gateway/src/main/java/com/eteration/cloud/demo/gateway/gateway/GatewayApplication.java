e6ec7622295901f2bfd030ac1d98f224f3516e84
Murat Karakas <murat.karakas@eteration.com>
2019-05-20 17:11:15 +0300

    clean up

--- gateway/src/main/java/com/eteration/cloud/demo/gateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/eteration/cloud/demo/gateway/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.eteration.cloud.demo.gateway.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableZuulProxy
